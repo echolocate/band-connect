a6c8e85be712bdcd11c5f0727e84227f6d1af26c
echolocate <markdance@gmail.com>
2021-11-29 14:12:48 +0000

    docker setu- commented out login

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -16,4 +16,4 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
+# docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
