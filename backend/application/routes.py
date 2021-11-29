b0cb5b9f13b442c94872f508ef044a3de308a4be
echolocate <markdance@gmail.com>
2021-11-29 21:06:06 +0000

    Progress so far

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -2,10 +2,18 @@ from application import app, db
 from application.models import Bands, Agent
 from flask import render_template, request, redirect, url_for, Response, jsonify
 
+@app.route('/create/agent', methods=['POST'])
+def create_agent():
+    package = request.json
+    new_agent = Agent(agent_name=package["agent_name"], Agent(phone=package["phone"]))
+    db.session.add(new_agent)
+    db.session.commit()
+    return Response(f"Added agent: {new_agent.name}", mimetype='text/plain')
+
 @app.route('/create/band', methods=['POST'])
 def create_band():
     package = request.json
-    new_band = Bands(name=package["name"])
+    new_band = Bands(name=package["name"], Bands(phone=package["phone"]))
     db.session.add(new_band)
     db.session.commit()
     return Response(f"Added band: {new_band.name}", mimetype='text/plain')
